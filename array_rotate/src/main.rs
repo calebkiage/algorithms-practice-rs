05260f98d45bc690a7aa2d6b8534f330b5a96909
Caleb Kiage <caleb.kiage@gmail.com>
2022-03-13 22:11:25 +0300

    Use juggling algorithm

--- array_rotate/src/main.rs
+++ array_rotate/src/main.rs
@@ -1,38 +1,71 @@
+// https://www.geeksforgeeks.org/array-rotation/
 fn main() {
-    let arr = [1, 2, 3, 4, 5, 6];
+    let mut arr = [1, 2, 3, 4, 5, 6];
     
     let factor = 7;
     
-    rotate(&arr, factor);
+    left_rotate(&mut arr, factor);
 }
 
-fn rotate(array: &[i32], factor: i32) -> &[i32] {
-    let append = if factor > 0 { false } else { true };
-    let length: usize = array.len();
-    // Remove sign
-    let mut f: usize = if factor < 0 { !factor + 1 } else { factor } as usize;
-    f = f % length;
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn left_rotate(array: &mut [i32], factor: usize) {
+    let length = array.len();
     if length < 1 {
-        return array;
+        return;
     }
-    let mut response: Vec<i32> = Vec::new();
-    
-    for i in 0..length {
-        if append {
-            if i < f {
-                response.push(array[i + length - f]);
-            } else {
-                response.push(array[i - f]);
+
+    let f = factor % length;
+    let gcd = gcd(f, length);
+    for i in 0..gcd {
+        let tmp = array[i];
+        let mut j = i;
+
+        loop {
+            let mut k = j + f;
+            if k >= length {
+                k -= length;
             }
-        } else {
-            if i < length - f {
-                response.push(array[i + f]);
-            } else {
-                response.push(array[i + f - length]);
+
+            if k == i {
+                break;
             }
+
+            array[j] = array[k];
+            j = k;
         }
-        println!("{}", response[i]);
+        array[j] = tmp;
     }
-    
-    return array;
+}
+
+#[cfg(test)]
+mod tests {
+    mod the_rotate_function {
+        use crate::left_rotate;
+
+        #[test]
+        fn should_return_original_array_given_a_single_element_array() {
+            let mut array = [2];
+
+            left_rotate(&mut array, 2);
+
+            assert_eq!(array[0], 2);
+        }
+
+        #[test]
+        fn should_return_rotated_array() {
+            let mut array = [1, 2, 3, 4, 5];
+
+            left_rotate(&mut array, 2);
+
+            assert_eq!(array[0], 3);
+            assert_eq!(array[3], 1);
+        }
+    }
 }
